591fa804598140f97e96e006029dbc6adee2d1d9
aleksei24 <lexa24032@gmail.com>
2020-02-07 10:57:24 +0300

    создан проект уроков jQuery

---
+++ assets/js/main.js
@@ -0,0 +1,97 @@
+$(function(){
+    // событие при клике на элемент
+    // $('#btn').on('click', function(){
+    //     // alert('Ouch!!! You`ve pushed me')
+    //     // prompt('I`m here to make your wishes true, master')
+    //     // confirm('Are you master?')
+    // });
+
+    // добавление класса, удаление класса, удаление блока, поиск элемента
+    // $('p').addClass('selected');
+    // $('#btn').addClass('selected');
+    // $('.bottom').addClass('selected');
+    // $('h3:has(span)').addClass('selected');
+    // $('div').removeClass();
+    // $('button').empty();
+    // $('div').find('p').addClass('selected');
+    
+    // переключение классов, плавное исчезновение и появление элементов
+    // $('button').on('click', function(){
+    //     // $('button').toggleClass('bg')
+    //     // $('#top').addClass('gold')
+    //     // $('.gold').fadeOut(2000).fadeIn(2500);
+    // });
+
+    // событие при наведении курсора на элемент
+    // $('h1').mouseover(function(){
+    //     $(this).fadeOut(1500);
+    // });
+
+    // событие при отведении курсора с элемента
+    // $('h1').mouseleave(function(){
+    //     $(this).fadeIn(3000);
+    // });
+
+    // анимация (добавление стилей) при клике на элемент
+    // $('button').on('click', function(){
+    //     $('#top').animate({
+    //         opacity: .5,
+    //         fontSize: '40px',
+    //         letterSpacing: '10px'
+    //     },2000);
+    // });
+
+    // простенький калькулятор, выводящий сумму двух введённых чисел
+    // $('#example-form').on('submit', function(event){
+    //     event.preventDefault();
+
+    //     let numOne, numTwo, sum;
+        
+    //     numOne = $('#one').val();
+    //     numTwo = $('#two').val();
+
+    //     numOne = parseInt(numOne);
+    //     numTwo = parseInt(numTwo);
+
+    //     if(isNaN(numOne, numTwo)){
+    //         numOne = 0;
+    //         numTwo = 0;
+    //     }
+
+    //     sum = numOne + numTwo;
+    //     $('#result').text(sum);
+    // });
+
+    // изменение аттрибута элемента (здесь: картинки) при клике на элемент
+    // let srcValue = $('#pic-inside img').attr('src');
+
+    // $('#pic-inside img').on('click', function(){
+    //     $(this).attr('src', 'img/yellow-car.png');
+    // });
+
+    // $('#colourSelector .colour-item').on('click', function(){
+    //     let imgPath = $(this).attr('data-image-path');
+    //     $('#pic-inside img').fadeOut(900, function(){
+    //         $('#pic-inside img').attr('src', imgPath).fadeIn(900);
+    //     });
+    // });
+
+    // добавление и удаление контента внутри элемента
+    // $('#test').text('New text');
+    // $('#test').html('<h3>New text</h3>');
+    // $('#no-wrap').on('click', function(){
+    //     $(this).empty();
+    // });
+
+    // удаление элемента
+    // $('#test').on('click', function(){
+    //     $(this).remove();
+    // });
+
+    // клонирование элемента
+    // $('#test').on('click', function(){
+    //     let cloneElement = $(this).clone();
+    //     $('#test').after(cloneElement);
+    // });
+
+});
